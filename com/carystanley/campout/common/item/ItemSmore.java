06a01799e710e35f56fc8b3c41129ffbad882525
Cary Stanley <cary@yahoo-inc.com>
2013-11-10 11:05:18 -0800

    Smores make you Hyper

--- com/carystanley/campout/common/item/ItemSmore.java
+++ com/carystanley/campout/common/item/ItemSmore.java
@@ -3,9 +3,13 @@ package com.carystanley.camping.common.item;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.item.ItemFood;
 
+import net.minecraft.potion.Potion;
+
 public class ItemSmore extends ItemFood {
     public ItemSmore(int id, int foodAmount, float sat, boolean isWolfMeat) {
         super(id, foodAmount, sat, isWolfMeat);
+        this.setPotionEffect(Potion.moveSpeed.getId(), 40, 4, 1.0f);
+        this.setAlwaysEdible();
     }
         
     @Override
