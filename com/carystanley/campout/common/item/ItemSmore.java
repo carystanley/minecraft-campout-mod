94c427051837fd3035c0263bcdd30cec205ca1f8
Cary Stanley <cary@yahoo-inc.com>
2013-11-10 16:41:26 -0800

    Smores can be fed to Mobs

--- com/carystanley/campout/common/item/ItemSmore.java
+++ com/carystanley/campout/common/item/ItemSmore.java
@@ -4,6 +4,12 @@ import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.item.ItemFood;
 
 import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+
 
 public class ItemSmore extends ItemFood {
     public ItemSmore(int id, int foodAmount, float sat, boolean isWolfMeat) {
@@ -16,5 +22,26 @@ public class ItemSmore extends ItemFood {
     public void registerIcons(IconRegister par1IconRegister)
     {
         this.itemIcon = par1IconRegister.registerIcon("campout:smore");
+    }
+
+    /**
+     * Returns true if the item can be used on the given entity, e.g. shears on sheep.
+     */
+    public boolean itemInteractionForEntity(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, EntityLivingBase par3EntityLivingBase)
+    {
+        par3EntityLivingBase.addPotionEffect(new PotionEffect(Potion.moveSpeed.getId(), 4000, 8));
+        --par1ItemStack.stackSize;
+
+        return true;
+    }
+
+    /**
+     * Current implementations of this method in child classes do not use the entry argument beside ev. They just raise
+     * the damage on the stack.
+     */
+    public boolean hitEntity(ItemStack par1ItemStack, EntityLivingBase par2EntityLivingBase, EntityLivingBase par3EntityLivingBase)
+    {
+        this.itemInteractionForEntity(par1ItemStack, (EntityPlayer)null, par2EntityLivingBase);
+        return true;
     }
 }
